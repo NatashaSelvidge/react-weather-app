6d12ef7649e1d7b6d40aea75c32134952cb2a2db
Natasha A Selvidge <natasha.selvidge@gmail.com>
2020-10-05 19:13:52 -0700

    cleaned up my code on App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,22 @@
 import React from "react";
-import "./styles.css";
 
-import "./Weather.css";
+import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="text-right p-5">
-        <form id="search-form">
-          <input type="search" id="city-input" placeholder="Enter a City" />
-          <button type="button" className="text-right" id="button-search">
-            <span role="img" aria-label="looking-glass">
-              {" "}
-              🔍{" "}
-            </span>{" "}
-            Search City
-          </button>
-        </form>
-      </div>
-
-      <div className="container">
-        <div className="border p-5" id="sky">
-          <h1 id="city">New York</h1>
-          <span className="Today" role="img">
-            Today{" "}
-            <span role="img" aria-label="looking-glass">
-              {" "}
-              ☀️{" "}
-            </span>
-            <span id="temperature"> 71 </span>
-            <a href="/" id="celsius-link">
-              {" "}
-              °C{" "}
-            </a>
-            |
-            <a href="/" id="fahrenheit-link">
-              °F{" "}
-            </a>
-          </span>
-
-          <div className="date">
-            <div className="text-right" id="day">
-              <h3>Wednesday July 1, 2020</h3>
-            </div>
-          </div>
-        </div>
-        <footer>
-          <a
-            href="https://github.com/SAC-CS112-Selvidge-Natasha/my-Weather-project"
-            rel="noopener norefferrer"
-            className="my-git"
-          >
-            Open source code by Natasha Selvidge
-          </a>
-        </footer>
-      </div>
+    <div className="App">
+      <Weather />
+      <footer>
+        <a
+          href="https://github.com/SAC-CS112-Selvidge-Natasha/my-Weather-project"
+          target="_blank"
+          className="my-git"
+          rel="noopener noreferrer"
+        >
+          Open source code on GitHub by Natasha Selvidge
+        </a>
+      </footer>
     </div>
   );
 }
